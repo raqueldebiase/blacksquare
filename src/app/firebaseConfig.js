8c30bda92b411fee7baf2b81a2c045f8561d8dff
Raquel De Biase <raqueldebiase@gmail.com>
2024-07-22 16:29:40 +0100

    add firebase auth

--- src/app/firebaseConfig.js
+++ src/app/firebaseConfig.js
@@ -1,10 +1,7 @@
-// firebaseConfig.js
-
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getAnalytics, isSupported } from 'firebase/analytics';
 
-// Obtendo as variáveis de ambiente
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -15,11 +12,9 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Inicialize o Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// Inicialize o Firebase Analytics somente no lado do cliente
 let analytics;
 if (typeof window !== 'undefined') {
   isSupported().then((supported) => {
