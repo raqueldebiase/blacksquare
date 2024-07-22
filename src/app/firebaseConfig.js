ea0812460a8582b88ba3d2a544aca14b22e66bc0
Raquel De Biase <raqueldebiase@gmail.com>
2024-07-22 16:23:18 +0100

    add firebase auth

--- src/app/firebaseConfig.js
+++ src/app/firebaseConfig.js
@@ -2,21 +2,31 @@
 
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getAnalytics } from 'firebase/analytics';
+import { getAnalytics, isSupported } from 'firebase/analytics';
 
+// Obtendo as variáveis de ambiente
 const firebaseConfig = {
-  apiKey: 'AIzaSyDLaxfisBXbXxBEg8qo2q8EzfZz_-e_5bU',
-  authDomain: 'blacksquare-68023.firebaseapp.com',
-  projectId: 'blacksquare-68023',
-  storageBucket: 'blacksquare-68023.appspot.com',
-  messagingSenderId: '774963391404',
-  appId: '1:774963391404:web:ff57f946c9c63c1f776809',
-  measurementId: 'G-XWQ37H5TL1',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+// Inicialize o Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
-export { auth, analytics }; // Certifique-se de exportar 'auth'
+// Inicialize o Firebase Analytics somente no lado do cliente
+let analytics;
+if (typeof window !== 'undefined') {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  });
+}
+
+export { auth, analytics };
